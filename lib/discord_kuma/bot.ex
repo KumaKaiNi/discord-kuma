d7b0636c34d4cad26f88a3658dacbc723c4472b1
rekyuu <senkan.re.kyuu@gmail.com>
2017-07-20 22:40:49 -0600

    fsdfjdsa

--- lib/discord_kuma/bot.ex
+++ lib/discord_kuma/bot.ex
@@ -115,6 +115,7 @@ defmodule DiscordKuma.Bot do
                 thumbnail: %Nostrum.Struct.Embed.Thumbnail{url: "#{user.logo}"},
                 timestamp: "#{DateTime.utc_now() |> DateTime.to_iso8601()}"
               }], chan: log_chan
+            end
         end
       end
     end
