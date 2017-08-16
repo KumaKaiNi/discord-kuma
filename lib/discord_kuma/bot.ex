4a4db8a9f11b419ce2037ce360158ad0c72f3d2e
rekyuu <senkan.re.kyuu@gmail.com>
2017-08-15 23:06:20 -0600

    stats

--- lib/discord_kuma/bot.ex
+++ lib/discord_kuma/bot.ex
@@ -58,6 +58,7 @@ defmodule DiscordKuma.Bot do
     match "!safe", :safebooru
     match "!jackpot", :get_jackpot
     match "!top5", :get_top_five
+    match "!stats", :get_stats
     match ["ty kuma", "thanks kuma", "thank you kuma"], :ty_kuma
     match_all :custom_command
 
