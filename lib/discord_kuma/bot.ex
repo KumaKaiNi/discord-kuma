dec12a9727356551f8af324cc0e5eef4b0e1981a
rekyuu <senkan.re.kyuu@gmail.com>
2017-08-15 20:37:22 -0600

    changed top10 to top5

--- lib/discord_kuma/bot.ex
+++ lib/discord_kuma/bot.ex
@@ -57,7 +57,7 @@ defmodule DiscordKuma.Bot do
     match "!quote", :get_quote
     match "!safe", :safebooru
     match "!jackpot", :get_jackpot
-    match "!top10", :get_top_five
+    match "!top5", :get_top_five
     match ["ty kuma", "thanks kuma", "thank you kuma"], :ty_kuma
     match_all :custom_command
 
@@ -378,18 +378,18 @@ defmodule DiscordKuma.Bot do
   def get_top_five(msg) do
     users = query_all_data(:bank)
 
-    top10 = for {username, coins} <- users do
+    top5 = for {username, coins} <- users do
       unless Enum.member?(["rekyuus", "kumakaini", "nightbot"], username) do
         {coins, username}
       end
     end |> Enum.sort |> Enum.reverse |> Enum.take(5)
 
-    top10_strings = for x <- 0..9 do
-      {:ok, {coins, username}} = Enum.fetch(top10, x)
+    top5_strings = for x <- 0..4 do
+      {:ok, {coins, username}} = Enum.fetch(top5, x)
       "#{x + 1}. #{username} (#{coins})"
     end
 
-    leaderboard = top10_strings |> Enum.join("\n")
+    leaderboard = top5_strings |> Enum.join("\n")
     reply "```\n#{leaderboard}\n```"
   end
 
