0cd729f28630e1e8f0bbab665554090ffeeb902e
rekyuu <senkan.re.kyuu@gmail.com>
2017-10-10 20:26:41 -0600

    server calls

--- lib/discord_kuma/bot.ex
+++ lib/discord_kuma/bot.ex
@@ -60,7 +60,7 @@ defmodule DiscordKuma.Bot do
           :ok ->
             case :gen_tcp.recv(socket, 0) do
               {:ok, response} ->
-                IO.inspect response
+                IO.inspect(response |> Poison.Parser.parse!(keys: :atoms))
 
                 case response |> Poison.Parser.parse!(keys: :atoms) do
                   %{reply: true, message: text} -> reply text
