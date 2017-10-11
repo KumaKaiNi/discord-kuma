a29618d367e107aa9c3b309562ca77b3ade87cc1
rekyuu <senkan.re.kyuu@gmail.com>
2017-10-10 19:26:10 -0600

    announces

--- lib/discord_kuma/module.ex
+++ lib/discord_kuma/module.ex
@@ -88,15 +88,15 @@ defmodule DiscordKuma.Module do
     end
   end
 
-  defmacro reply(text) when is_bitstring(text) do
-    quote do
-      Channel.send_message(var!(state)[:rest_client], var!(msg).data["channel_id"], %{content: unquote(text)})
-    end
-  end
-
   defmacro reply(struct, chan: channel_id) when is_map(struct) do
     quote do
       Channel.send_message(var!(state)[:rest_client], unquote(channel_id), unquote(struct))
+    end
+  end
+
+  defmacro reply(text) when is_bitstring(text) do
+    quote do
+      Channel.send_message(var!(state)[:rest_client], var!(msg).data["channel_id"], %{content: unquote(text)})
     end
   end
 
