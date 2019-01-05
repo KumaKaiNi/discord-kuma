692a84b49c78de6ad6b2d9f3ec367f7fa5f82fe5
rekyuu <senkan.re.kyuu@gmail.com>
2019-01-04 21:26:01 -0700

    oops haha

--- lib/discord_kuma/tourney.ex
+++ lib/discord_kuma/tourney.ex
@@ -5,7 +5,7 @@ defmodule DiscordKuma.Tourney do
 
     cond do
       participants_count <= 2 -> participants
-      true -> for chunk <- Enum.chunk_every(participants, rounds) do
+      true -> for chunk <- Enum.chunk(participants, rounds) do
         bracket(chunk)
       end
     end
